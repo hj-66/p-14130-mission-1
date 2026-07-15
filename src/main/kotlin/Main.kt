33e03a5e800866d906b4815ca9cab4847f406c91
hj-66 <a01088200534@gmail.com>
2026-07-15 11:47:45 +0900

    5단계

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,19 +4,30 @@ package com
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
     var count = 1;
+    val wiseSayings: MutableList<String> = mutableListOf()
+    val authors: MutableList<String> = mutableListOf()
 
     println("== 명언 앱 ==")
     while (true) {
         print("명령) ")
-        var command = readLine()
+        val command = readLine()
         when (command) {
             "등록" -> {
                 print("명언 : ")
-                var wiseSaying = readLine()
+                val wiseSaying: String? = readLine()
+                wiseSayings.add(wiseSaying ?: "")
                 print("작가 : ")
-                var author = readLine()
+                val author: String? = readLine()
+                authors.add(author ?: "")
                 println("${count}번 명언이 등록되었습니다.")
                 count++
+            }
+            "목록" -> {
+                println("번호 / 작가 / 명언")
+                println("----------------------")
+                wiseSayings.withIndex().reversed().forEach { (index, wiseSaying) ->
+                    println("${index + 1} / ${authors.get(index)} / $wiseSaying")
+                }
             }
             else -> break
         }
