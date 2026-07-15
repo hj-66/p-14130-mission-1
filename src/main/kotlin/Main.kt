dffe7fa4e2ed7310368163a46242aa031b8a9aa8
hj-66 <a01088200534@gmail.com>
2026-07-15 11:33:30 +0900

    4단계

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,11 +3,23 @@ package com
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
+    var count = 1;
+
     println("== 명언 앱 ==")
-    print("명언 : ")
-    var wiseSaying = readLine()
-    print("작가 : ")
-    var author = readLine()
-    println("1번 명언이 등록되었습니다.")
+    while (true) {
+        print("명령) ")
+        var command = readLine()
+        when (command) {
+            "등록" -> {
+                print("명언 : ")
+                var wiseSaying = readLine()
+                print("작가 : ")
+                var author = readLine()
+                println("${count}번 명언이 등록되었습니다.")
+                count++
+            }
+            else -> break
+        }
+    }
     println("명령) 종료")
 }
