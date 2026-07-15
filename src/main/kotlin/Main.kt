543edd8321f033a07f0af4cf120e5ec4684119f2
hj-66 <a01088200534@gmail.com>
2026-07-15 12:38:00 +0900

    8단계

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,11 +1,13 @@
 package com
 
+import jdk.jfr.internal.consumer.EventLog.update
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
     var count = 1;
-    val wiseSayings: MutableList<String> = mutableListOf()
-    val authors: MutableList<String> = mutableListOf()
+    val wiseSayings: MutableList<String?> = mutableListOf()
+    val authors: MutableList<String?> = mutableListOf()
 
     println("== 명언 앱 ==")
     while (true) {
@@ -22,23 +24,50 @@ fun main() {
                 println("${count}번 명언이 등록되었습니다.")
                 count++
             }
+
             "목록" -> {
                 println("번호 / 작가 / 명언")
                 println("----------------------")
-                wiseSayings.withIndex().reversed().forEach { (index, wiseSaying) ->
-                    println("${index + 1} / ${authors.get(index)} / $wiseSaying")
+                val copyOfWiseSayings: List<String?> = wiseSayings.toList()
+                copyOfWiseSayings.withIndex().reversed().forEach { (index, wiseSaying) ->
+                    if (copyOfWiseSayings.get(index) != null) {
+                        println("${index + 1} / ${authors.get(index)} / $wiseSaying")
+                    }
                 }
             }
+
             "삭제" -> {
-                val deleteIndex: Int? = command.split("?id=")[1].toIntOrNull()
-                if (deleteIndex in wiseSayings.indices) {
-                    wiseSayings.removeAt(deleteIndex ?: 0)
-                    authors.removeAt(deleteIndex ?: 0)
-                    println("${deleteIndex}번 명언이 삭제되었습니다.")
+                val deleteIndex: Int? = command.split("?id=")[1].toIntOrNull()?.minus(1)
+                if (deleteIndex == null || deleteIndex !in wiseSayings.indices
+                    || wiseSayings.get(deleteIndex) == null
+                ) {
+                    println("${deleteIndex?.plus(1)}번 명언은 존재하지 않습니다.")
                 } else {
-                    println("${deleteIndex}번 명언은 존재하지 않습니다.")
+                    wiseSayings.set(deleteIndex, null)
+                    authors.set(deleteIndex, null)
+                    println("${deleteIndex?.plus(1)}번 명언이 삭제되었습니다.")
                 }
             }
+
+            "수정" -> {
+                val updateIndex: Int? = command.split("?id=")[1].toIntOrNull()?.minus(1)
+                if (updateIndex == null || updateIndex !in wiseSayings.indices
+                    || wiseSayings.get(updateIndex) == null
+                ) {
+                    println("${updateIndex?.plus(1)}번 명언은 존재하지 않습니다.")
+                } else {
+                    println("명언(기존) : ${wiseSayings.get(updateIndex)}")
+                    print("명언 : ")
+                    val updateWiseSaying: String? = readLine()
+                    println("작가(기존): ${authors.get(updateIndex)}")
+                    print("작가 : ")
+                    val updateAuthor: String? = readLine()
+                    wiseSayings.set(updateIndex, updateWiseSaying ?: "")
+                    authors.set(updateIndex, updateAuthor ?: "")
+                    println("${updateIndex?.plus(1)}번 명언이 수정되었습니다.")
+                }
+            }
+
             "종료" -> break
             else -> println("명령을 다시 입력해 주세요")
         }
